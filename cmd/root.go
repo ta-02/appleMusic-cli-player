9f32a011282abf289c6d4cd7b24c9f5009f4b5e8
ta-02 <talal.gl2004@gmail.com>
2024-06-29 21:24:54 -0400

    Updated docs for normal command

--- cmd/root.go
+++ cmd/root.go
@@ -9,13 +9,9 @@ import (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "music",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "CLI for controlling music playback",
+	Long: `Music is a command-line interface (CLI) tool that allows you to control music playback.
+It leverages Cobra, a CLI library for Go, to define commands and manage application flags.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
