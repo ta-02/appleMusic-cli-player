c20c7f38945d4366ba617a3d64896f9e3dbf5577
ta-02 <talal.gl2004@gmail.com>
2024-06-29 20:19:57 -0400

    added pause

---
+++ cmd/current.go
@@ -0,0 +1,41 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+)
+
+type fullSongInfo struct {
+	trackName  string
+	artistName string
+}
+
+func showCurrent()
+
+var currentCmd = &cobra.Command{
+	Use:   "current",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("current called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(currentCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// currentCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// currentCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
