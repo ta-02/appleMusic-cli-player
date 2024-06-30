9434f962266022d96e91ef76217ca368b6ce5314
ta-02 <talal.gl2004@gmail.com>
2024-06-30 14:51:02 -0400

    added volume cmd

---
+++ cmd/playlists.go
@@ -0,0 +1,29 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+func showPlaylist() {
+
+}
+
+var playlistsCmd = &cobra.Command{
+	Use:   "playlists",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("playlists called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(playlistsCmd)
+}
---
+++ cmd/volume.go
@@ -0,0 +1,48 @@
+package cmd
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/andybrewer/mack"
+	"github.com/spf13/cobra"
+)
+
+var volumeCmd = &cobra.Command{
+	Use:   "volume",
+	Short: "Set the volume for Apple Music",
+	Long: `Set the volume for Apple Music. Usage example:
+
+music volume 50
+This will set the Apple Music volume to 50%.`,
+
+	Args: cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		input := args[0]
+		vol, err := strconv.Atoi(input)
+		if err != nil || vol < 1 || vol > 100 {
+			fmt.Println("Please enter a number from 1-100")
+			return
+		}
+
+		isOpen, err := isMusicOpen()
+		if err != nil {
+			fmt.Println("Error checking if Apple Music is open:", err)
+			return
+		}
+
+		if !isOpen {
+			fmt.Println("Apple Music is not open!")
+			return
+		}
+
+		script := fmt.Sprintf("set sound volume to %d", vol)
+		if _, err := mack.Tell("Music", script); err != nil {
+			fmt.Println("Error setting volume:", err)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(volumeCmd)
+}
