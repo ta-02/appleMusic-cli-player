c9e190ba95ca5b990b10f5330ec0cf35cc98d321
ta-02 <talal.gl2004@gmail.com>
2024-06-30 15:49:11 -0400

    added playlist fzf finder

--- cmd/playlists.go
+++ cmd/playlists.go
@@ -2,12 +2,25 @@ package cmd
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
+	"strings"
 
+	"github.com/andybrewer/mack"
 	"github.com/spf13/cobra"
 )
 
-func showPlaylist() {
-
+func showPlaylist() ([]string, error) {
+	script := `
+    set playlistNames to name of playlists
+    return playlistNames
+  `
+	result, err := mack.Tell("Music", script)
+	if err != nil {
+		return nil, fmt.Errorf("error fetching playlists: %w", err)
+	}
+	playlistNames := strings.Split(result, ", ")
+	return playlistNames, nil
 }
 
 var playlistsCmd = &cobra.Command{
@@ -21,11 +34,44 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Args: cobra.RangeArgs(0, 1),
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("playlists called")
+		isOpen, err := isMusicOpen()
+		if err != nil {
+			fmt.Println("Error checking if Apple Music is open:", err)
+			return
+		}
+
+		if !isOpen {
+			fmt.Println("Apple Music is not open!")
+			return
+		}
+
 		if len(args) == 0 {
+			playlistNames, err := showPlaylist()
+			if err != nil {
+				fmt.Println("Error gettings playlust names:", err)
+				return
+			}
 
+			if len(playlistNames) == 0 {
+				fmt.Println("No playlists found.")
+				return
+			}
+
+			fzfCmd := exec.Command("fzf")
+			fzfCmd.Stdin = strings.NewReader(strings.Join(playlistNames, "\n"))
+			// fzfCmd.Stdout = os.Stdout
+			fzfCmd.Stderr = os.Stderr
+
+			playlistBytes, err := fzfCmd.Output()
+			if err != nil {
+				fmt.Println("Error running fzf:", err)
+				return
+			}
+
+			selectedPlaylist := strings.TrimSpace(string(playlistBytes))
+			fmt.Println("Selected Playlist:", selectedPlaylist)
 		} else {
-
+			return
 		}
 	},
 }
